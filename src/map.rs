dca9901f1f3db33cb4dc166dabdb20dfee62b336
KingOfMongeese <nwohletz@gmail.com>
2025-04-28 20:03:58 -0600

    chore: clean up

--- src/map.rs
+++ src/map.rs
@@ -41,35 +41,4 @@ impl Map {
         }
     }
 
-    /// renders the map on screen
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(BASE_LAYER);
-        for y in camera.top_y..camera.bottom_y {
-            for x in camera.left_x..camera.right_x {
-                if self.in_bounds(Point::new(x, y)) {
-                    let idx = get_map_idx(x, y);
-                    match self.tiles[idx] {
-                        TileType::Floor => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                DARKGRAY,
-                                BLACK,
-                                to_cp437('.'),
-                            );
-                        }
-                        TileType::Wall => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                DARK_GRAY,
-                                GRAY,
-                                to_cp437('#'),
-                            );
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
