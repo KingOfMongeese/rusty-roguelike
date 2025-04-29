2860b6daa3184b85bbc2d6fc06198bfa390db7a5
KingOfMongeese <nwohletz@gmail.com>
2025-04-28 20:01:14 -0600

    feat: systems

---
+++ src/systems/entity_render.rs
@@ -0,0 +1,24 @@
+use std::mem::offset_of;
+
+use crate::{camera, prelude::*};
+
+#[system]
+#[read_component(Point)]
+#[read_component(Render)]
+pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
+    let mut draw_batch = DrawBatch::new();
+    draw_batch.target(ENTITY_LAYER);
+    let offset = Point::new(camera.left_x, camera.top_y);
+
+    <(&Point, &Render)>::query()
+        .iter(ecs)
+        .for_each(|(pos, render)|{
+            draw_batch.set(
+                *pos - offset,
+                render.color,
+                render.glyph
+            );
+        });
+
+    draw_batch.submit(5000).expect("Batch Error");
+}
