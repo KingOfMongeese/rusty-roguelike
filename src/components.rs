59a831046a3e6606138ddc9fff2b1d3256df8ce6
KingOfMongeese <nwohletz@gmail.com>
2025-05-18 17:23:10 -0600

    feat: add collision detection

--- src/components.rs
+++ src/components.rs
@@ -1,13 +1,16 @@
 pub use crate::prelude::*; // TODO: find out why pub use here
 
+/// contains render data
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
     pub color: ColorPair,    // color pair stores fg and bg, from bracket
     pub glyph: FontCharType, // bracket, store char or glyph
 }
 
+/// tags Player
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
 
+/// tags Enemy
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
