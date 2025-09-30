8b5327e34d9739e2d349e10ae6a1d9768c06c47b
kingofmongeese <nwohletz@gmail.com>
2025-09-30 13:02:58 -0600

    feat: wait to heal

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -17,6 +19,8 @@ pub fn player_input(
             VirtualKeyCode::D => Point::new(1, 0),
             VirtualKeyCode::W => Point::new(0, -1),
             VirtualKeyCode::S => Point::new(0, 1),
+
+            // non movement to wait and heal
             _ => Point::new(0, 0),
         };
         // pg 153
@@ -26,6 +30,7 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -33,6 +38,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -42,6 +48,7 @@ pub fn player_input(
                     ));
                 });
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -49,6 +56,16 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+        }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
 
