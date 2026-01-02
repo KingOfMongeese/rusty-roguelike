6252441951b760deb81d64b24ff6620937fac09b
kingofmongeese <nwohletz@gmail.com>
2026-01-02 15:43:46 -0700

    fmt

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -13,45 +13,44 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .map(|(entity, attack_msg)| (*entity, attack_msg.attacker, attack_msg.defender))
         .collect();
 
-    attacks
-        .iter()
-        .for_each(|(message, attacker, defender)| {
-            // get a ref to the defender components
-            // check if player is 1 of those components
-            let is_player = ecs
-                .entry_ref(*defender)
-                .unwrap()
-                .get_component::<Player>()
-                .is_ok();
+    attacks.iter().for_each(|(message, attacker, defender)| {
+        // get a ref to the defender components
+        // check if player is 1 of those components
+        let is_player = ecs
+            .entry_ref(*defender)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
 
-            let base_damage = if let Ok(attacker_ref) = ecs.entry_mut(*attacker) {
-                if let Ok(dmg) = attacker_ref.get_component::<Damage>() {
-                    dmg.0
-                } else {
-                    0
-                }
+        let base_damage = if let Ok(attacker_ref) = ecs.entry_mut(*attacker) {
+            if let Ok(dmg) = attacker_ref.get_component::<Damage>() {
+                dmg.0
             } else {
                 0
-            };
-
-            let weapon_damage: i32 = <(&Carried, &Damage)>::query().iter(ecs)
-                .filter(|(carried, _)| carried.0 == *attacker)
-                .map(|(_, dmg)| dmg.0)
-                .sum();
-
-            let final_damge = base_damage + weapon_damage;
-
-            if let Ok(health) = ecs
-                .entry_mut(*defender)
-                .unwrap()
-                .get_component_mut::<Health>()
-            {
-                health.current -= final_damge;
-                if health.current < 1 && !is_player {
-                    commands.remove(*defender);
-                }
             }
+        } else {
+            0
+        };
 
-            commands.remove(*message);
-        });
+        let weapon_damage: i32 = <(&Carried, &Damage)>::query()
+            .iter(ecs)
+            .filter(|(carried, _)| carried.0 == *attacker)
+            .map(|(_, dmg)| dmg.0)
+            .sum();
+
+        let final_damge = base_damage + weapon_damage;
+
+        if let Ok(health) = ecs
+            .entry_mut(*defender)
+            .unwrap()
+            .get_component_mut::<Health>()
+        {
+            health.current -= final_damge;
+            if health.current < 1 && !is_player {
+                commands.remove(*defender);
+            }
+        }
+
+        commands.remove(*message);
+    });
 }
